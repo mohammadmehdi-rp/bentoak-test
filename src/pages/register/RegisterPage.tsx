4bb13acc68129f05280dc599b82afb985674647c
mohammad mehdi rajabpour <mohammadmehdirajabpour2@gmail.com>
2023-12-07 21:34:39 +0330

    feat: add RegisterPage ui

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,7 +1,82 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { AuthLayout } from "layouts";
 
 function RegisterPage() {
-  return <AuthLayout>Hi</AuthLayout>;
+  return (
+    <AuthLayout>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Box component="form" onSubmit={() => {}} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="fullName"
+            label="FullName"
+            name="fullName"
+            autoComplete="fullName"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 3 }}
+          >
+            Submit
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Link href="#" variant="body2">
+              {"Already have an account? Login"}
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </AuthLayout>
+  );
 }
 
 export default RegisterPage;
